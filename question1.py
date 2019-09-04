d09418a6dad87b3d6b3c960fb993ad1339fb2cc4
Kenn <kenn+git@code-crew.org>
2019-09-03 20:11:53 -0500

    All questions answered

---
+++ question1.py
@@ -0,0 +1,5 @@
+# Ask a user for the year they were born by calculating their age. Assuming they already had their birthday and it’s 2019 print “You are [AGE] years old”
+
+userInput = int(input("Enter the year you were born: "))
+ageOfPerson = 2019-userInput
+print("You are " + str(ageOfPerson))
